964d9b9ea91fb69763937216cca0f5cd68794897
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-03-22 17:01:35 -0300

    novo teste

--- src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
+++ src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@EnableJpaRepositories
 class SmartsuplifyApplicationTests {
 
 	@Test
